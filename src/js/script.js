1004252c19919bf03658b33dd7e31c78eaa4a211
netdang <dangkhmernupp8@gmail.com>
2025-05-22 19:49:17 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  toggleButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+});
